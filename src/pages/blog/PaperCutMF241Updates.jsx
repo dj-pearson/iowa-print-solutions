6677debef11cdcc331a09ed524f8d8ebefb51144
dj-pearson <pearson.performance@gmail.com>
2025-07-24 09:45:06 -0500

    feat: add React import to PaperCutMF241Updates component

--- src/pages/blog/PaperCutMF241Updates.jsx
+++ src/pages/blog/PaperCutMF241Updates.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { motion } from 'framer-motion'
 import { AlertCircle, Shield, Server, CheckCircle, ArrowRight, DollarSign, Users, Globe } from 'lucide-react'
 import { Link } from 'react-router-dom'
